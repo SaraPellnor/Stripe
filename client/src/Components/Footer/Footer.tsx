aebcc6aab75482eb562b94f6efd0477369696ae1
Sara Pellnor <114143422+SaraPellnor@users.noreply.github.com>
2023-09-20 17:11:56 +0200

    footer och productcard

--- client/src/Components/Footer/Footer.tsx
+++ client/src/Components/Footer/Footer.tsx
@@ -3,26 +3,24 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="container">
-        <div className="contact-info">
-          <h3>Kontakta oss</h3>
-          <p>Har du frågor? Skicka oss ett meddelande:</p>
-          <input
-            type="text"
-            placeholder="Skriv din fråga här"
-            className="input"
-          />
-          <button className="submit-button">Skicka</button>
-        </div>
-        <div className="additional-info">
-          <h3>Övrig information</h3>
-          <ul>
-            <li>Om oss</li>
-            <li>Kundtjänst</li>
-            <li>Frakt och leverans</li>
-            <li>Returer och byten</li>
-          </ul>
-        </div>
+      <div className="contact-info">
+        <h3>Kontakta oss</h3>
+        <p>Har du frågor? Skicka oss ett meddelande:</p>
+        <input
+          type="text"
+          placeholder="Skriv din fråga här"
+          className="input"
+        />
+        <button className="submit-button">Skicka</button>
+      </div>
+      <div className="info">
+        <h3>Övrig information</h3>
+        <ul>
+          <li>Om oss</li>
+          <li>Kundtjänst</li>
+          <li>Frakt och leverans</li>
+          <li>Returer och byten</li>
+        </ul>
       </div>
     </footer>
   );
